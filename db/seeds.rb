62b8169866fe03d87ceb247e994b58b90a781abe
Adrian Ramirez <adrianramirez7@Adrians-MacBook-Pro.local>
2023-10-29 16:51:25 -0700

    backend structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+sharks = [
+    {
+        name: "Bruce",
+        age: 8,
+        enjoys: "eating clown fish",
+        image: ''
+    },
+    {
+        name: 'Flan',
+        age: 3,
+        enjoys: 'Laying on the sea floor',
+        image: ''
+    },
+    {
+        name: 'sharkboy',
+        age: 15,
+        enjoys: 'Dressing up as a shark human',
+        image: ''
+    }
+]
+
+sharks.each do |shark|
+    Shark.create shark
+    puts "creating shark #{shark}"
+end
